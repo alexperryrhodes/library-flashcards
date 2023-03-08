82fd8a73537036189625d6435aa4c5225fc57ece
Alexandra Rhodes <atperry30@gmail.com>
2023-03-08 13:54:05 -0800

    Created card data and imported components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import CardList from "./components/CardList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState(cardData);
 
   return (
     <div className="App">
-      
+      <Header />
+      <CardList cards={cards} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const cardData = [
+  {
+    id: 0,
+    data: {
+      front: "quote0",
+      back: "author0",
+    },
+  },
+  {
+    id: 1,
+    data: {
+      front: "quote1",
+      back: "author1",
+    },
+  },
+  {
+    id: 2,
+    data: {
+      front: "quote2",
+      back: "author2",
+    },
+  },
+  {
+    id: 3,
+    data: {
+      front: "quote3",
+      back: "author4",
+    },
+  },
+];
