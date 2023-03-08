b5a5faa2d7281e2c88c6b35d9fed9023cfc2bb5c
Alexandra Rhodes <atperry30@gmail.com>
2023-03-08 13:54:40 -0800

    Created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>Who Said...?</h1>
+            <h3>Test your knowledge of famous literary quotes</h3>
+            <p></p>
+        </div>
+    );
+};
+
+export default Header;
