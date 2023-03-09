b33621dd475bb4adef21cce91597b67d2794cff8
Alexandra Rhodes <atperry30@gmail.com>
2023-03-08 20:36:35 -0800

    Added more detail to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const Header = () => {
-    return (
-        <div className="Header">
-            <h1>Who Said...?</h1>
-            <h3>Test your knowledge of famous literary quotes</h3>
-            <p></p>
-        </div>
-    );
+const Header = ({ cardCount }) => {
+  return (
+    <div className="Header">
+      <h1>Who Said...?</h1>
+      <h3>Test your knowledge of famous literary quotes!</h3>
+          <p>For each quote below, name the book the quote is from</p>
+          <p>For extra bonus points, see if you can name the author as well!</p>
+          <p>Total Cards: { cardCount }</p>
+    </div>
+  );
 };
 
 export default Header;
