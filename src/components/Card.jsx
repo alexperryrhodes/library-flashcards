70345b61081b6119aff7c1a3e2b925f19ad10ed1
Alexandra Rhodes <atperry30@gmail.com>
2023-03-08 20:37:21 -0800

    Updated card to toggle based on state

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,9 @@ import React, { useState } from "react";
 const Card = ({ card }) => {
   const [isCardFront, setIsCardFront] = useState(true);
   const cardClass = `card ${isCardFront ? "front" : "back"}`;
-    return (
+  return (
     <div className={cardClass} onClick={() => setIsCardFront(!isCardFront)}>
-      {/* <div className="front">{card.front}</div>
-      <div className="back">{card.back}</div> */}
-            {isCardFront ? card.front : card.back}
-            {card.id}
+      {isCardFront ? card.data.front : card.data.back}
     </div>
   );
 };
